b5a7b4e19810f2cb9b09cf71e72d36b5a1dedbab
ahmed <ahmedalzoghrory@gmail.com>
2025-09-10 02:13:33 +0300

    feat: Implement Delete functionality

--- resources/views/shop/products.blade.php
+++ resources/views/shop/products.blade.php
@@ -18,13 +18,13 @@
     <div class="row">
         @foreach ($products as $product)
             <div class="col-4">
-                {{-- الصورة: تأكد أن عندك حقل image_path في DB أو ضع صورة افتراضية --}}
+                
                 <img src="{{ asset($product->image_path ?? 'images/default.jpg') }}" alt="{{ $product->name }}">
                 
                 <h4>{{ $product->name }}</h4>
                 
                 <div class="rating">
-                    {{-- تقدر تخزن تقييم المنتج في DB وتعرضه، هنا ثابت مثال --}}
+                   
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
                     <i class="fa fa-star"></i>
@@ -34,14 +34,14 @@
                 
                 <p>${{ number_format($product->price, 2) }}</p>
                 
-                {{-- رابط لصفحة تفاصيل المنتج --}}
+               
                 <a href="{{ route('products.show', $product->id) }}" class="btn">View Details</a>
             </div>
         @endforeach
     </div>
 
     <div class="page-btn">
-        {{-- روابط الترقيم --}}
+        
         {{ $products->links() }}
     </div>
 </div>
